1de2a8bffce107bd8dcb31468a7b4767a6062a12
rolandbudai <budairoland@gmail.com>
2021-02-21 01:45:33 +0100

    chng

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fri = document.querySelector('.fri');
 
 //change background color of day icons - red/green
 
-let colors =['#44bd32', '#c23616'];
+let colors =['#44bd32', '#c23616', 'rgb(191, 196, 196)'];
 let m = 0;
 let t = 0;
 let w = 0;
